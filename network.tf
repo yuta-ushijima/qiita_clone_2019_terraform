728c558bde167c4f83f61be7721ff144b40def70
Yuta Ushijima <register@yuta-u.com>
2019-08-12 08:18:58 +0900

    Add resource of qiita_clone_2019_rds_subnet

--- network.tf
+++ network.tf
@@ -24,3 +24,12 @@ resource "aws_subnet" "qiita_clone_2019_private_subnet" {
     "Name" = "qiita_clone_2019_private_subnet"
   }
 }
+
+resource "aws_subnet" "qiita_clone_2019_rds_subnet" {
+  availability_zone = "ap-northeast-1a"
+  cidr_block        = "10.0.2.0/24"
+  vpc_id            = "${aws_vpc.qiita_clone_2019_vpc.id}"
+  tags = {
+    "Name" = "qiita_clone_2019_rds_subnet"
+  }
+}
