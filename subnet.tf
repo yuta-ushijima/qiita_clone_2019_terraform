0d6381ed6846ec23105c6b1648adeb45ff280c08
Yuta Ushijima <register@yuta-u.com>
2019-08-12 08:21:10 +0900

    fix: rename from network.tf to subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -1,12 +1,3 @@
-resource "aws_vpc" "qiita_clone_2019_vpc" {
-  // デフォルト値は記載せず定義必須なパラメータやデフォルト値から変更したパラメータを定義
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_hostnames = false
-  tags = {
-    "Name" = "qiita_clone_2019_vpc"
-  }
-}
-
 resource "aws_subnet" "qiita_clone_2019_public_subnet" {
   availability_zone = "ap-northeast-1d"
   cidr_block        = "10.0.0.0/24"
