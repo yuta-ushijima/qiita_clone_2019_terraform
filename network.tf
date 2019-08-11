3345d7cf49cfcfcfd1cd463bec89de282cbfaec9
Yuta Ushijima <register@yuta-u.com>
2019-08-11 23:20:08 +0900

    fix: execute: terraform fmt

--- network.tf
+++ network.tf
@@ -1,8 +1,8 @@
 resource "aws_vpc" "qiita_clone_2019_vpc" {
   // デフォルト値は記載せず定義必須なパラメータやデフォルト値から変更したパラメータを定義
-  cidr_block                       = "10.0.0.0/16"
-  enable_dns_hostnames             = false
-  tags                             = {
-        "Name" = "qiita_clone_2019_vpc"
-    }
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = false
+  tags = {
+    "Name" = "qiita_clone_2019_vpc"
+  }
 }
