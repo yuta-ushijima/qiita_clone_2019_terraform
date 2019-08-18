48a4aec26aa89886f818c36a2d9cf6a745450d39
Yuta Ushijima <register@yuta-u.com>
2019-08-18 22:45:22 +0900

    fix: Modified contents of security_group.tf

--- security_group.tf
+++ security_group.tf
@@ -1,102 +1,10 @@
 resource "aws_security_group" "qiita_clone_2019_sg" {
   name        = "qiita_clone_2019_sg"
   description = "qiita_clone_2019_sg"
-
-  egress = [
-    {
-      cidr_blocks = [
-        "0.0.0.0/0",
-      ]
-      description      = ""
-      from_port        = 0
-      ipv6_cidr_blocks = []
-      prefix_list_ids  = []
-      protocol         = "-1"
-      security_groups  = []
-      self             = false
-      to_port          = 0
-    },
-  ]
-  ingress = [
-    {
-      cidr_blocks = [
-        "0.0.0.0/0",
-      ]
-      description = ""
-      from_port   = 3000
-      ipv6_cidr_blocks = [
-        "::/0",
-      ]
-      prefix_list_ids = []
-      protocol        = "tcp"
-      security_groups = []
-      self            = false
-      to_port         = 3000
-    },
-    {
-      cidr_blocks = [
-        "0.0.0.0/0",
-      ]
-      description = ""
-      from_port   = 80
-      ipv6_cidr_blocks = [
-        "::/0",
-      ]
-      prefix_list_ids = []
-      protocol        = "tcp"
-      security_groups = []
-      self            = false
-      to_port         = 80
-    },
-    {
-      cidr_blocks = [
-        "27.83.59.109/32",
-        "0.0.0.0/0",
-      ]
-      description      = ""
-      from_port        = 22
-      ipv6_cidr_blocks = []
-      prefix_list_ids  = []
-      protocol         = "tcp"
-      security_groups  = []
-      self             = false
-      to_port          = 22
-    },
-  ]
   vpc_id = "${aws_vpc.qiita_clone_2019_vpc.id}"
 }
 
 resource "aws_security_group" "qiita_clone_2019_rds_security_group" {
   description = "qiita_clone_2019_rds_security_group"
-  egress = [
-    {
-      cidr_blocks = [
-        "0.0.0.0/0",
-      ]
-      description      = ""
-      from_port        = 0
-      ipv6_cidr_blocks = []
-      prefix_list_ids  = []
-      protocol         = "-1"
-      security_groups  = []
-      self             = false
-      to_port          = 0
-    },
-  ]
-  ingress = [
-    {
-      cidr_blocks = [
-        "0.0.0.0/0",
-      ]
-      description      = ""
-      from_port        = 3306
-      ipv6_cidr_blocks = []
-      prefix_list_ids  = []
-      protocol         = "tcp"
-      security_groups  = []
-      self             = false
-      to_port          = 3306
-    },
-  ]
   vpc_id = "${aws_vpc.qiita_clone_2019_vpc.id}"
 }
