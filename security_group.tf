5fdd92f01a3228311706f191b8e3e56c20eec575
Yuta Ushijima <register@yuta-u.com>
2019-08-25 17:07:11 +0900

    hotfix: execute terraform fmt

--- security_group.tf
+++ security_group.tf
@@ -1,10 +1,10 @@
 resource "aws_security_group" "qiita_clone_2019_sg" {
   name        = "qiita_clone_2019_sg"
   description = "qiita_clone_2019_sg"
-  vpc_id = "${aws_vpc.qiita_clone_2019_vpc.id}"
+  vpc_id      = "${aws_vpc.qiita_clone_2019_vpc.id}"
 }
 
 resource "aws_security_group" "qiita_clone_2019_rds_security_group" {
   description = "qiita_clone_2019_rds_security_group"
-  vpc_id = "${aws_vpc.qiita_clone_2019_vpc.id}"
+  vpc_id      = "${aws_vpc.qiita_clone_2019_vpc.id}"
 }
